ca5d40feeae85a1bca6a0c496fe48c208d7a84f7
kris.o <yajkakusta@gmail.com>
2021-10-28 20:20:53 +0500

    magic file

--- example_008/Program.cs
+++ example_008/Program.cs
@@ -2,12 +2,12 @@
 //Console.SetCursorPosition(значение 1,значение 2);
 //Console.WriteLine(+); - этим символом закрасим 
 
-int xa = 40, ya = 1,
+int xa = 40, ya = 1, //ввод переменных и значений для них
     xb = 1,  yb = 30,
     xc = 80, yc = 30;
 
-Console.SetCursorPosition(xa, ya);
-Console.WriteLine("+");
+Console.SetCursorPosition(xa, ya); //обозначение курсора
+Console.WriteLine("+");            // вывод на консоль положения курсора. и так с каждой переменной
 
 Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+");
@@ -16,18 +16,18 @@ Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
 
-int x = xa, y = ya;
+int x = xa, y = ya; //ввод новой переменной
 
-int count = 0;
+int count = 0;      //ввод count - количество повторений
 
-while (count < 10000)
+while (count < 10000) //цикл while (пока count < 10000)
 {
      int what = new Random().Next(0,3); // 0,1,2 - размер;
 
-if (what == 0)
+if (what == 0)       // цикл if - если переменная what (рандомное число в массиве от 0 до 3) = , то выполнять след. д-ие:
     {
-    x=(x + xa)/2;
-    y=(y + ya)/2;
+    x=(x + xa)/2; // тело цикла
+    y=(y + ya)/2;  // тело цикла
     } 
 
 if (what == 1)
@@ -42,7 +42,7 @@ if (what == 2)
     y=(y + yc)/2;
     } 
 
-  Console.SetCursorPosition(x,y);
-  Console.WriteLine("+");
-  count = count+1;
+  Console.SetCursorPosition(x,y); // позиция на консоли переменных
+  Console.WriteLine("+"); // обозначение и рисунок
+  count = count+1; // увеличение на 1 для движения цикла. цикл while остановится, когда count = 10000
 } 
